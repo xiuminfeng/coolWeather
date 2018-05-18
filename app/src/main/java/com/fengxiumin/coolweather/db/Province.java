9d3f4ccc1c1f587772dcceb712013ba71686632a
fengxiumin <xiuminfeng@qq.com>
2018-05-18 19:03:52 +0800

    加入创建数据库和表的各项配置
    git commit -m 加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/fengxiumin/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.fengxiumin.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Traunig on 2018/5/18.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
